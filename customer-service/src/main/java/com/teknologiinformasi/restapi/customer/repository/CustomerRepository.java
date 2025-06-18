b6ac61d958a5a01e5721eaed25e94d8af1da41a6
M.Haikal Habibillah <168246758+mhaikalhabibillah@users.noreply.github.com>
2025-06-18 18:04:49 +0700

    Delete customer-service directory

--- customer-service/src/main/java/com/teknologiinformasi/restapi/customer/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teknologiinformasi.restapi.customer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.teknologiinformasi.restapi.customer.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
